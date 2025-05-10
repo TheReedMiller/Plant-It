9ad55bd90f9444c6df575dea4349e5a942d7c38b
Reed Edward Miller <reedmiller1025@gmail.com>
2025-05-09 20:19:48 -0400

    Added Basic Fern Class

---
+++ GameLib/Fern.cpp
@@ -0,0 +1,35 @@
+/**
+ * @file Fern.cpp
+ * @author Reed Miller
+ */
+
+#include "pch.h"
+#include "Fern.h"
+#include "Game.h"
+
+
+/**
+ * Constructor
+ * @param game Game this Flytrap belongs to
+ */
+Fern::Fern(Game* game) : Plant(game, L"fern.png")
+{
+    //Simple up-call
+}
+
+/**
+ * Function to save the state of this Fern
+ * @param gameNode root node of XML
+ * @return a completed xmlNode for this object
+ */
+wxXmlNode* Fern::Save(wxXmlNode *gameNode)
+{
+    //Up-call to item class
+    auto itemNode = Item::Save(gameNode);
+
+    //Add type of plant
+    itemNode->AddAttribute(L"type", L"fern");
+
+    //Return the Node
+    return itemNode;
+}
---
+++ GameLib/Fern.h
@@ -0,0 +1,27 @@
+/**
+ * @file Fern.h
+ * @author Reed Miller
+ *
+ *
+ */
+ 
+#ifndef FERN_H
+#define FERN_H
+
+#include "Plant.h"
+
+
+/**
+ * A Class that Describes A fern Plant
+ */
+class Fern : public Plant{
+private:
+
+public:
+    Fern(Game* game);
+    wxXmlNode* Save(wxXmlNode* gameNode);
+};
+
+
+
+#endif //FERN_H

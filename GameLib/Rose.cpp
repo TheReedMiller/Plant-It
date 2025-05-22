412a57dd4025b096d9ba8a35b5492f8882e79024
Reed Edward Miller <reedmiller1025@gmail.com>
2025-05-21 21:30:04 -0400

    Rose can now be toggled

--- GameLib/Rose.cpp
+++ GameLib/Rose.cpp
@@ -6,11 +6,16 @@
 #include "pch.h"
 #include "Rose.h"
 
+///Constant for Watered FileName
+const std::wstring RegRoseImage = L"rose_1.png";
+///Constant for Dry FileName
+const std::wstring DryRoseImage = L"rose_2.png";
+
 /**
  * Constructor
  * @param game Game this rose belongs to
  */
-Rose::Rose(Game* game) : Plant(game, L"rose_1.png")
+Rose::Rose(Game* game) : Plant(game, RegRoseImage)
 {
     //Simple up-call
 }
@@ -37,5 +42,26 @@ wxXmlNode* Rose::Save(wxXmlNode *gameNode)
  */
 void Rose::Click()
 {
+    //First we get the state
+    auto state = GetState();
 
+    //State is currently watered, flip it
+    if (state == L"watered")
+    {
+        //Set New State
+        SetState(L"dry");
+
+        //Set new image and Bitmap
+        SetImage(DryRoseImage);
+    }
+
+    //Otherwise state is Dry, flip it
+    else
+    {
+        //Set New State
+        SetState(L"watered");
+
+        //Set ne Image and Bitmap
+        SetImage(RegRoseImage);
+    }
 }

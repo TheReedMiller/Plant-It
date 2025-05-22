501309febb21e8127e0a2916e7fdec8b1cb57e95
Reed Edward Miller <reedmiller1025@gmail.com>
2025-05-21 21:24:04 -0400

    Fern can now be toggled to dry

--- GameLib/Fern.cpp
+++ GameLib/Fern.cpp
@@ -7,12 +7,16 @@
 #include "Fern.h"
 #include "Game.h"
 
+///Constant for Watered FileName
+const std::wstring RegFernImage = L"fern_1.png";
+///Constant for Dry FileName
+const std::wstring DryFernImage = L"fern_2.png";
 
 /**
  * Constructor
  * @param game Game this Flytrap belongs to
  */
-Fern::Fern(Game* game) : Plant(game, L"fern_1.png")
+Fern::Fern(Game* game) : Plant(game, RegFernImage)
 {
     //Simple up-call
 }
@@ -39,5 +43,26 @@ wxXmlNode* Fern::Save(wxXmlNode *gameNode)
  */
 void Fern::Click()
 {
+    //First we get the state
+    auto state = GetState();
 
+    //State is currently watered, flip it
+    if (state == L"watered")
+    {
+        //Set New State
+        SetState(L"dry");
+
+        //Set new image and Bitmap
+        SetImage(DryFernImage);
+    }
+
+    //Otherwise state is Dry, flip it
+    else
+    {
+        //Set New State
+        SetState(L"watered");
+
+        //Set ne Image and Bitmap
+        SetImage(RegFernImage);
+    }
 }

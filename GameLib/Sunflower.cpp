b16aa2a9e86b2ac51fca434460823a282e58f8cc
Reed Edward Miller <reedmiller1025@gmail.com>
2025-05-26 10:23:48 -0400

    Fixed Bug where a dry sunflower wasn't loaded in properly

--- GameLib/Sunflower.cpp
+++ GameLib/Sunflower.cpp
@@ -74,3 +74,22 @@ void Sunflower::Toggle()
         SetLevel(0);
     }
 }
+
+/**
+ * Function to Load in the state of this Sunflower
+ * @param childNode node to load Xml data from
+ */
+void Sunflower::Load(wxXmlNode* childNode)
+{
+    //Get State Attribute
+    auto state = childNode->GetAttribute(L"state");
+
+    //If state is Dry, toggle it
+    if (state == L"dry")
+    {
+        Toggle();
+    }
+
+    //Call to Plant Class to load
+    Plant::Load(childNode);
+}
